5ccf0193557d39a0f266a777b17692498a1ebbb0
Dante <jhonalexanderfonseca1993@gmail.com>
2019-12-13 17:20:38 -0500

    CRUD de personas

---
+++ src/main/java/com/example/democrud/model/Persona.java
@@ -0,0 +1,66 @@
+package com.example.democrud.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String apelllido;
+
+    @Column
+    private String direccion;
+
+    @Column
+    private String telefono;
+
+    public Persona() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApelllido() {
+        return apelllido;
+    }
+
+    public void setApelllido(String apelllido) {
+        this.apelllido = apelllido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ src/main/java/com/example/democrud/service/api/PersonaServiceAPI.java
@@ -0,0 +1,10 @@
+package com.example.democrud.service.api;
+
+import com.example.democrud.commons.GenereicServiceAPI;
+import com.example.democrud.model.Persona;
+
+public interface PersonaServiceAPI extends GenereicServiceAPI<Persona,Long> {
+
+
+
+}
